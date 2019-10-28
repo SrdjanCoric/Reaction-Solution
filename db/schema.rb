027b88716df1cb0f78d051701aeb1da91c9ee367
SrdjanCoric <srdjan.coric1984@gmail.com>
2019-10-28 13:46:46 +0100

    update seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_124100) do
+ActiveRecord::Schema.define(version: 2019_10_28_124552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_10_28_124100) do
     t.text "description"
     t.string "labels", default: [], array: true
     t.integer "list_id"
+    t.datetime "due_date"
   end
 
   create_table "lists", force: :cascade do |t|
